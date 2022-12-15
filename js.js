a2d8f6c871e4daadf6cd72409667a5a5e86d1819
VituD2 <Jvitor_134@outlook.com>
2022-12-15 19:16:34 -0300

    1st commit

---
+++ js.js
@@ -0,0 +1,6 @@
+let button_1 = document.querySelector(".button");
+
+button_1.addEventListener("click", () => {
+    window.open("https://open.spotify.com/track/76bcaYeKOKTrj9lEc9BZFU?si=06364f9f2eba47e2", target="_blank");
+    window.open("sec_page.html", target="_self");
+});
