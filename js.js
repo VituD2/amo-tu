fb969140bfd22af3e6b8207262d53afef5c0b51e
VituD2 <Jvitor_134@outlook.com>
2022-12-16 21:59:36 -0300

    2nd commit

--- js.js
+++ js.js
@@ -1,6 +1,12 @@
 let button_1 = document.querySelector(".button");
 
 button_1.addEventListener("click", () => {
-    window.open("https://open.spotify.com/track/76bcaYeKOKTrj9lEc9BZFU?si=06364f9f2eba47e2", target="_blank");
+    // window.open("https://open.spotify.com/track/76bcaYeKOKTrj9lEc9BZFU?si=06364f9f2eba47e2", target="_blank");
     window.open("sec_page.html", target="_self");
+});
+
+let spotify_button = document.querySelector('.spotify-button');
+
+spotify_button.addEventListener('click', () => {
+    window.open("https://open.spotify.com/track/76bcaYeKOKTrj9lEc9BZFU?si=06364f9f2eba47e2", target="_blank");
 });
---
+++ js2.js
@@ -0,0 +1,18 @@
+// let hearts = document.querySelector('#hearts-div');
+// let main_img = document.querySelectorAll('#main-img');
+// let sec_img = document.querySelectorAll('#sec-img');
+
+// hearts.addEventListener('mouseenter', () => {
+//     for
+// });
+
+let jv_heart = document.querySelector('.jv-heart');
+let iana_heart = document.querySelector('.iana-heart');
+
+jv_heart.addEventListener('click', () => {
+    window.open('https://instagram.com/vituud2', target="_blank");
+})
+
+iana_heart.addEventListener('click', () => {
+    window.open('https://www.instagram.com/https_ianaa/', target="_blank");
+})
