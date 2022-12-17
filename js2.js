0a3e7d205e7ea19256dde365aeb8e63c40607c45
VituD2 <Jvitor_134@outlook.com>
2022-12-17 08:40:04 -0300

    4th commit

--- js2.js
+++ js2.js
@@ -1,13 +1,39 @@
-// let hearts = document.querySelector('#hearts-div');
-// let main_img = document.querySelectorAll('#main-img');
-// let sec_img = document.querySelectorAll('#sec-img');
-
-// hearts.addEventListener('mouseenter', () => {
-//     for
-// });
-
 let jv_heart = document.querySelector('.jv-heart');
 let iana_heart = document.querySelector('.iana-heart');
+
+let jv_main_img = document.querySelector('#jv-main-img');
+let jv_sec_img = document.querySelector('#jv-sec-img');
+
+let iana_main_img = document.querySelector('#iana-main-img');
+let iana_sec_img = document.querySelector('#iana-sec-img');
+
+
+jv_heart.addEventListener('mouseover', () => {
+    jv_main_img.classList.add('off-img');
+    jv_sec_img.classList.remove('off-img');
+});
+
+jv_heart.addEventListener('mouseout', () => {
+    jv_sec_img.classList.add('off-img');
+    jv_main_img.classList.remove('off-img');
+    }
+);
+
+
+iana_heart.addEventListener('mouseover', () => {
+    iana_main_img.classList.add('off-img');
+    iana_sec_img.classList.remove('off-img');
+});
+
+iana_heart.addEventListener('mouseout', () => {
+    iana_sec_img.classList.add('off-img');
+    iana_main_img.classList.remove('off-img');
+    }
+);
+
+
+
+
 
 jv_heart.addEventListener('click', () => {
     window.open('https://instagram.com/vituud2', target="_blank");
